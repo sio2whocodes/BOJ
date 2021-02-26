91adc49fbee83adb90edac05c3984a2b1b6a7a89
hello-SiOO <fltcy2039@naver.com>
2021-02-26 17:48:26 +0900

    2021.02.26

---
+++ implement/implement/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  implement
+//
+//  Created by 임수정 on 2021/02/26.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main(){
+    int roomNum;
+    cin >> roomNum;
+    vector<int> needNum(10,0);
+    if(roomNum == 0){
+        cout << 1;
+    }
+    while(roomNum != 0){
+        needNum[roomNum % 10]++;
+        roomNum /= 10;
+    }
+    needNum[6] = ceil((needNum[6]+needNum[9])/2.0);
+    needNum[9] = 0;
+
+    int max = needNum[0];
+    for(int i = 1 ; i <= 9 ; i++){
+        if(max < needNum[i])
+            max = needNum[i];
+    }
+    cout << max;
+    return 0;
+}
