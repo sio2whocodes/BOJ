6d43121820a4707b217c9ce2c9c7d1b3401482c9
sujeong lim <20180998@sungshin.ac.kr>
2021-07-12 16:55:26 +0900

    210712 pq 1

--- priority_queue/priority_queue/1655.cpp
+++ priority_queue/priority_queue/1655.cpp
@@ -4,27 +4,51 @@
 //
 //  Created by 임수정 on 2021/06/29.
 //
+//https://sio2whocode.tistory.com/124
+//https://www.acmicpc.net/problem/1655
 
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <cmath>
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    priority_queue<int, vector<int>, greater<>> heap;
+    priority_queue<int, vector<int>, greater<>> min_heap;
+    priority_queue<int, vector<int>, less<>> max_heap;
 
-    int t;
-    cin >> t;
+    int N;
+    cin >> N;
     
-    int x;
-    for(int i = 0 ; i < t ; i++){
-        cin >> x;
-        heap.push(x);
-//        heap.
+    for(int i = 0 ; i < N ; i++){
+        int n;
+        cin >> n;
+        //n input
+        if(max_heap.empty()){
+            max_heap.push(n);
+        }else{
+            if(n < max_heap.top()){
+                max_heap.push(n);
+            }else{
+                min_heap.push(n);
+            }
+        }
+        
+        if(max_heap.size() - min_heap.size() == 2){
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }else if(min_heap.size() - max_heap.size() == 2){
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+        }
+        //peek
+        if(max_heap.size() == min_heap.size() || min_heap.size() < max_heap.size()){
+            cout << max_heap.top() << "\n";
+        }else if(max_heap.size() < min_heap.size()){
+            cout << min_heap.top() << "\n";
+        }
     }
     
     return 0;
