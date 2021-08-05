90e2c4e6f5e1f679e69b1268116d4199c5ebf2cb
sujeong lim <20180998@sungshin.ac.kr>
2021-08-05 16:08:39 +0900

    BFS LCA 문제풀이

--- dfsNbfs/dfsNbfs/11437.cpp
+++ dfsNbfs/dfsNbfs/11437.cpp
@@ -4,13 +4,18 @@
 //
 //  Created by 임수정 on 2021/08/04.
 //
+//https://sio2whocode.tistory.com/135
 
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-//int adj[50000][50000];
 vector<vector<int>> adj(50001);
+int parents[50001];
+int depths[50001];
+int visit[50001];
+queue<int> q;
 
 int main(){
     //input & init
@@ -21,18 +26,53 @@ int main(){
         int p, c;
         cin >> p >> c;
         adj[p].push_back(c);
+        adj[c].push_back(p);
     }
     
-    for(int i = 1 ; i <= N ; i++){
-        cout << i << ">";
-        for(int j = 0 ; j < adj[i].size() ; j++){
-            cout << adj[i][j] << ", ";
+    //bfs : 부모노드, 깊이 계산
+    parents[1] = 0;
+    depths[1] = 1;
+    q.push(1);
+    while(!q.empty()){
+        int parent = q.front();
+        q.pop();
+        visit[parent] = 1;
+        for(int i = 0 ; i < adj[parent].size() ; i++){
+            int child = adj[parent][i];
+            if(!visit[child]){
+                parents[child] = parent;
+                depths[child] = depths[parent]+1;
+                q.push(child);
+            }
         }
-        cout << "\n";
     }
     
+    //공통조상 탐색
+    //input
+    int M;
+    cin >> M;
     
-    
+    for(int i = 0 ; i < M ; i++){
+        int a, b;
+        cin >> a >> b;
+        int result = 1;
+        while(true){
+            if(a == b){
+                result = a;
+                break;
+            }else{
+                if(depths[a] > depths[b]){
+                    a = parents[a];
+                }else if(depths[a] < depths[b]){
+                    b = parents[b];
+                }else{
+                    a = parents[a];
+                    b = parents[b];
+                }
+            }
+        }
+        cout << result << "\n";
+    }
     
     return 0;
 }
