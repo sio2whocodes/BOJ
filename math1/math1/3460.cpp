080ea586115f52b70d321542c08b900f63b6b7c4
sio2whocodes <fltcy2039@naver.com>
2024-07-23 22:45:12 +0900

    3460 이진수 풀이

---
+++ math1/math1/3460.cpp
@@ -0,0 +1,31 @@
+//
+//  3460.cpp
+//  math1
+//
+//  Created by sujeong on 7/23/24.
+//  Copyright © 2024 임수정. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+        
+    for(int i = 0 ; i < T ; i++){
+        int n;
+        cin >> n;
+        
+        int idx = 0;
+        while(n > 0){
+            if(n % 2 == 1){
+                cout << idx << " ";
+            }
+            n /= 2;
+            idx++;
+        }
+    }
+    
+    return 0;
+}
